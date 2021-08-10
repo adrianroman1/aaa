b2e1d686f52c6dbd00f7e37707af74ac6db5a8ed
adrianroman1 <88735805+adrianroman1@users.noreply.github.com>
2021-08-10 22:31:01 +0300

    add recommended and dbaccess tests + some refactoring

--- src/main/java/ru/aaa/test/configuration/ConfigurationException.java
+++ src/main/java/ru/aaa/test/configuration/ConfigurationException.java
@@ -1,1 +1,23 @@
+package ru.aaa.test.configuration;
 
+/**
+ * @author adrian_român
+ */
+public class ConfigurationException extends RuntimeException {
+	
+    public ConfigurationException() {
+    }
+
+    public ConfigurationException(String message) {
+        super(message);
+    }
+
+    public ConfigurationException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public ConfigurationException(Throwable cause) {
+        super(cause);
+    }
+
+}
