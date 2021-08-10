8e8c501a7a81610fe070e3f924a393711d858fe9
adrianroman1 <88735805+adrianroman1@users.noreply.github.com>
2021-08-10 22:38:23 +0300

    add recommended and dbaccess tests + some refactoring

--- src/main/java/ru/aaa/test/utils/Config.java
+++ src/main/java/ru/aaa/test/utils/Config.java
@@ -1,1 +1,27 @@
+package ru.aaa.test.utils;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.MessageSource;
+
+import java.util.Locale;
+
+/**
+ * @author adrian_român
+ */
+public class Config {
+
+    private Logger log = LoggerFactory.getLogger(Config.class);
+
+    private MessageSource messageSource;
+
+    public void setMessageSource(MessageSource messageSource) {
+        this.messageSource = messageSource;
+    }
+
+    public synchronized String getValue(String key) {
+        String value = messageSource.getMessage(key, null, Locale.US);
+        log.debug("Value: [" + value + "] with key: [" + key + "] was obtained from the *.properties file.");
+        return value;
+    }
+}
